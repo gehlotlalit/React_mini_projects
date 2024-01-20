96d6f720beb20e96f26280e052438bba3f199e5a
Lalit Malviya <gehlotlalit786@gmail.com>
2024-01-20 22:18:48 +0530

    first react

---
+++ basic-react-app/src/Product.jsx
@@ -0,0 +1,13 @@
+import "./Product.css";
+// import Price from "./Price";
+
+function Product(){
+    return (
+        <div className="Product">
+         <p>Title</p>
+
+        </div>
+    );
+}
+
+export default Product;
